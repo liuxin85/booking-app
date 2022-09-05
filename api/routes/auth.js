2b835f22e6772badd17a450de92bc16e73233df7
liuxin <liuxin85@gmail.com>
2022-09-05 15:25:47 +0800

    CRUD hotel

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.send("Hello, this is auth route");
 });
 
+router.get("/register", (req, res) => {
+  res.send("heloo, this is reigiste endpoint");
+});
+
 export default router;
